dcc5de092d4fc71106548ceef05ec8be304239e5
Daniel Lochrie <dclochri@gmail.com>
2015-09-18 13:39:03 -0500

    Initiall import.

---
+++ js/gameJQ.js
@@ -0,0 +1,34 @@
+var game = new Game;
+
+$('.game').click(function(){
+	game.start();
+	var playerAName = game.players.playerA.name;
+	var playerBName = game.players.playerB.name;
+	var parentEl = $('.player');
+	var attackerID = $(parentEl).attr('id');
+	var attacker = game.players[attackerID];
+});
+
+$('.hit').click(function(){
+	var parentEl = $(this).parent();
+	var attackerID = $(parentEl).attr('id');
+	var attacker = game.players[attackerID];
+	var opponent = attackerID === 'playerA' ? game.players['playerB'] :
+			game.players['playerA'];
+	var damage = attacker.hitPower;
+	opponent.life -= damage;
+	game.condition(attacker,opponent);
+	game.updateStats();
+})
+
+$('.kick').click(function(){
+	var parentEl = $(this).parent();
+	var attackerID = $(parentEl).attr('id');
+	var attacker = game.players[attackerID];
+	var opponent = attackerID === 'playerA' ? game.players['playerB'] :
+			game.players['playerA'];
+	var damage = attacker.kickPower;
+	opponent.life -= damage;
+	game.condition(attacker,opponent);
+	game.updateStats();
+});
